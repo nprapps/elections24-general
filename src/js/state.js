e972944a71623ba62a6499e8c00850dd11dafcec
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-10-11 12:01:15 -0700

    add nav functionality

--- src/js/state.js
+++ src/js/state.js
@@ -4,3 +4,11 @@ require("./components/results-table");
 let section = document.querySelector('input[name="nav"]:checked').value;
 
 let nav = document.querySelector("form");
+
+nav.addEventListener("change", e => {
+	const selectedSection = document.querySelector("#" + e.target.value + "-section");
+	document.querySelectorAll("section").forEach(section => {
+		section.classList.remove("shown");
+	});
+	selectedSection.classList.add("shown");
+})
