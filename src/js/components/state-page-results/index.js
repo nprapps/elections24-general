743e1d8dbeccb7850cd38c2a8647ed9e58da7ba9
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-10-27 22:37:55 -0700

    load data in state-page-results instead of results-collection!

--- src/js/components/state-page-results/index.js
+++ src/js/components/state-page-results/index.js
@@ -15,21 +15,24 @@ class StatePageResults extends ElementBase {
   constructor() {
     super();
     this.sections = JSON.parse(this.getAttribute("sections"));
+    this.keyRaceCollections = JSON.parse(this.getAttribute("key-race-collections"));
+    this.countyRaces = JSON.parse(this.getAttribute("county-races"));
+    this.state = this.getAttribute("state");
     this.loadData = this.loadData.bind(this);
   }
 
   connectedCallback() {
     this.loadData();
-    gopher.watch("./data/states/" + this.getAttribute("state") + ".json", this.loadData);
+    gopher.watch("./data/states/" + this.state + ".json", this.loadData);
   }
 
   disconnectedCallback() {
-    gopher.unwatch("./data/states/" + this.getAttribute("state") + ".json", this.loadData);
+    gopher.unwatch("./data/states/" + this.state + ".json", this.loadData);
   }
 
   async loadData() {
     try {
-      const response = await fetch("./data/states/" + this.getAttribute("state") + ".json");
+      const response = await fetch("./data/states/" + this.state + ".json");
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -42,73 +45,76 @@ class StatePageResults extends ElementBase {
 
   render() {
     this.removeAttribute("sections");
+    this.removeAttribute("key-race-collections");
+    this.removeAttribute("county-races");
+    
     const results = this.data.results;
     let template = "";
-
-    let keyRaceCollections = ["president"];
-    if (results.filter(d => d.office === "G").length > 0) {
-      keyRaceCollections.push("governor");
-    }
-    if (results.filter(d => d.office === "S").length > 0) {
-      keyRaceCollections.push("senate");
-    }
-    if (results.filter(d => d.office === "H" && d.keyRace === "yes").length > 0) {
-      keyRaceCollections.push("house");
-    }
-    if (results.filter(d => d.office === "I" && d.keyRace === "yes").length > 0) {
-      keyRaceCollections.push("ballot-measures");
-    }
 
     this.sections.forEach(section => {
       let sectionHTML = "";
 
       if (section === "key-races") {
-        sectionHTML += `
-          <section id="key-races-section" section="key-races" class="shown">
-        `
-        keyRaceCollections.forEach(office => {
+        sectionHTML += '<section id="key-races-section" section="key-races" class="shown">';
+        this.keyRaceCollections.forEach(office => {
+          let races = results.filter(d => {
+            if (office === "H" || office === "I") {
+              return d.office === offices[office] && d.keyRace === "yes";
+            } else {
+              return d.office === offices[office];
+            }
+          });
           sectionHTML += `
-            <results-collection state="${this.getAttribute('state')}" office="${offices[office]}" key-races-only></results-collection>
+            <results-collection state="${this.state}" office="${offices[office]}" races='${JSON.stringify(races).replace(/'/g, "&#39;")}' key-races-only></results-collection>
           `
         });
         sectionHTML += "</section>"
       } else {
+        let races = results.filter(d => d.office === offices[section]);
+
         if (section === "president") {
           sectionHTML += `
             <section id="${section}-section" section="${section}">
-              <results-collection state="${this.getAttribute("state")}" office="${offices[section]}"></results-collection>
+              <results-collection state="${this.state}" office="${offices[section]}" races='${JSON.stringify(races).replace(/'/g, "&#39;")}'></results-collection>
               <h3 class="section-hed">Presidential results by county</h3>
-              <county-map state="${this.getAttribute("state")}"></county-map>
-              <county-dataviz state="${this.getAttribute("state")}"></county-dataviz>
+              <county-map state="${this.state}"></county-map>
+              <county-dataviz state="${this.state}"></county-dataviz>
               <results-table-county
-                state="${this.getAttribute("state")}"
+                state="${this.state}"
                 race-id="0"
                 order="1">
               </results-table-county>
             </section>
           `
         } else if (section === "senate" || section === "governor") {
+          let countiesHTML = "";
+          let countyRaces = this.countyRaces.filter(d => d.office === offices[section]);
+          races.forEach(race => {
+            const countyHTML = `
+              <county-map state="${this.state}" race-id="${this.state}-${race.id}"></county-map>
+              <county-dataviz state="${this.state}" race="${this.state}-${race.id}"></county-dataviz>
+              <results-table-county
+                state="${this.state}"
+                race-id="${race.id}"
+                order="1">
+              </results-table-county>
+            `
+            countiesHTML += countyHTML;
+          });
+
           sectionHTML += `
             <section id="${section}-section" section="${section}">
-              <results-collection state="${this.getAttribute("state")}" office="${offices[section]}"></results-collection>
-              <h3 class="section-hed">${section.toUpperCase} results by county</h3>
-              <% listOfCountyRaces.filter(race => race.office === offices[section]).forEach((race, index) => { %>
-                <%= race.office %>: <%= race.id %>
-                <county-map state="<%= key %>" race-id="<%= key %>-<%= race.id %>"></county-map>
-                <county-dataviz state="<%= key %>" race="<%= key %>-<%= race.id %>"></county-dataviz>
-                <results-table-county
-                  state="<%= key %>"
-                  race-id="<%= race.id %>"
-                  order="1">
-              </results-table-county>
-            </section>
+              <results-collection state="${this.state}" office="${offices[section]}" races='${JSON.stringify(races).replace(/'/g, "&#39;")}'></results-collection>
+              <h3 class="section-hed">${section.charAt(0).toUpperCase() + section.slice(1)} results by county</h3>
           `
+          sectionHTML += countiesHTML;
+          sectionHTML += "</section>"
         } else {
           sectionHTML += `
             <section id="${section}-section" section="${section}">
-              <results-collection state="${this.getAttribute("state")}" office="${offices[section]}"></results-collection>
+              <results-collection state="${this.state}" office="${offices[section]}" races='${JSON.stringify(races).replace(/'/g, "&#39;")}'></results-collection>
             </section>
-            `
+          `
         }
       }
 
