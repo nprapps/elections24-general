206a7fa862b79d154708945c1d55b9803d2bd436
jadolphe <jadolphe@npr.org>
2024-10-29 12:47:51 -0400

    changed timing

--- src/js/components/board-president/index.js
+++ src/js/components/board-president/index.js
@@ -264,7 +264,7 @@ Geography
       if (this.tabButtons.length > 0) {
         this.updateTabSelection(this.tabButtons[this.initialSelectedTab]);
       }
-    }, 50);
+    }, 150);
   }
 
   updateTabSelection(clickedTab) {
